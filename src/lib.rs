b5a72b36e8e868fe65ab3d8eeecaeb60bcf8ed43
酿泉 <tioover@gmail.com>
2016-11-21 17:24:52 +0800

    Uniforms implement macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-extern crate glium;
+pub extern crate glium;
 extern crate cgmath;
 
 mod types;
@@ -9,3 +9,17 @@ pub mod mesh;
 pub use graphics::{Graphics, Gfx, Shader, Renderer};
 pub use mesh::Mesh;
 pub use types::{Mat4, Scalar};
+
+#[macro_export]
+macro_rules! implement_uniforms {
+    ($tyname: ty, $( $field: ident ),*) => {
+        impl $crate::glium::uniforms::Uniforms for $tyname {
+            fn visit_values<'a, F>(&'a self, mut f: F)
+                where F: FnMut(&str, $crate::glium::uniforms::UniformValue<'a>)
+            {
+                use $crate::glium::uniforms::AsUniformValue;
+                $(f(stringify!($field), (&self.$field).as_uniform_value());)*
+            }
+        }
+    };
+}
